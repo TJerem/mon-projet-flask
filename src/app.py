602d40a556b924db70090fba24a6ce66354b2dfd
tjerem <jeremy.tanasi@outlook.fr>
2026-04-14 15:38:55 +0200

    Création du projet Flask_V2

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ def home():
     return jsonify({"message": "Bienvenue sur mon API", "status": "ok"})
 @app.route("/health")
 def health():
-return jsonify({"status": "healthy"})
+    return jsonify({"status": "healthy"})
 @app.route("/hello/<name>")
 def hello(name):
-return jsonify({"message": f"Bonjour {name} !"})
+    return jsonify({"message": f"Bonjour {name} !"})
 if __name__ == "__main__":
-app.run(debug=True)
+    app.run(debug=True)
